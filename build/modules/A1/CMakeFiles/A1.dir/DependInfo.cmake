7a4385c124fc52b72ff3ccd3fc5bd5716cc7e19d
uufornunu <1411952270@qq.com>
2025-07-19 17:07:03 +0800

    提交CMAKE作业

---
+++ build/modules/A1/CMakeFiles/A1.dir/DependInfo.cmake
@@ -0,0 +1,25 @@
+
+# Consider dependencies only in project.
+set(CMAKE_DEPENDS_IN_PROJECT_ONLY OFF)
+
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  )
+
+# The set of dependency files which are needed:
+set(CMAKE_DEPENDS_DEPENDENCY_FILES
+  "/workspaces/codespaces-blank/CMake_Test/CMake1/modules/A1/src/A11.cpp" "modules/A1/CMakeFiles/A1.dir/src/A11.cpp.o" "gcc" "modules/A1/CMakeFiles/A1.dir/src/A11.cpp.o.d"
+  "/workspaces/codespaces-blank/CMake_Test/CMake1/modules/A1/src/A12.cpp" "modules/A1/CMakeFiles/A1.dir/src/A12.cpp.o" "gcc" "modules/A1/CMakeFiles/A1.dir/src/A12.cpp.o.d"
+  "/workspaces/codespaces-blank/CMake_Test/CMake1/modules/A1/src/A13.cpp" "modules/A1/CMakeFiles/A1.dir/src/A13.cpp.o" "gcc" "modules/A1/CMakeFiles/A1.dir/src/A13.cpp.o.d"
+  )
+
+# Targets to which this target links which contain Fortran sources.
+set(CMAKE_Fortran_TARGET_LINKED_INFO_FILES
+  )
+
+# Targets to which this target links which contain Fortran sources.
+set(CMAKE_Fortran_TARGET_FORWARD_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
